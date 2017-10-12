f4d94592bc7a6a32c409b9e857b4becc5c71aa34
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-10-12 16:18:09 -0700

    Add license header to source files

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2017 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const t = require('assert');
 
 test('dummy test', () => {
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/** Copyright (c) 2017 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // NOTE: prevailing assumption is a release's target_commitish is always a SHA.
 
 const generateChangelog = require('./generate-changelog');
